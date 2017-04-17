dc996fe4d267d2cd58f2d122b439e162987a9f17
shaikathaque <shaikathaque4@gmail.com>
2017-04-17 16:19:14 -0400

    Added LUIS

--- app.js
+++ app.js
@@ -1,24 +1,38 @@
 var builder = require('botbuilder');
 var restify = require('restify');
 
+// setup restify server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log('%s listening to %s', server.name, server.url);
+});
+
 // create the connector
 var connector = new builder.ChatConnector();
 
 //create the bot
 var bot = new builder.UniversalBot(connector);
 
-//add in the dialog
-bot.dialog('/', [
+server.post('/api/messages', connector.listen());
+
+
+//=====================================
+// Bots Dialogs
+//=====================================
+
+var recognizer = new builder.LuisRecognizer('https://westus.api.cognitive.microsoft.com/luis/v2.0/apps/e56356c4-1ade-49f3-abf4-5f1846e223ee?subscription-key=9aefdc1486b744049db504427816d708');
+bot.recognizer(recognizer);
+
+bot.dialog('start', function(session) {
+    session.send('Hello Broker!');
+    session.beginDialog('productInfo');
+}).triggerAction({matches: 'greetings'});
+
+bot.dialog('productInfo', [
     function(session) {
-        builder.Prompts.text(session, 'What is your name?');
+        builder.Prompts.text(session, 'What product are you shipping?');
     },
     function(session, results) {
-        session.send('Hello, ' + results.response);
+        session.send('You are shipping a ' + results.response);
     }
 ]);
-
-var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 3978, function() {
-    console.log('%s listening to %s', server.name, server.url);
-});
-server.post('/api/messages', connector.listen());
